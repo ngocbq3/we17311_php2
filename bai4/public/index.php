0eed83b344a04e7d1359a46a5fabbbb8dd38d443
ngocbq3 <bqngoc@gmail.com>
2023-02-07 13:32:32 +0700

    update htaccess

--- bai4/public/index.php
+++ bai4/public/index.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Models\CategoryModel;
+use App\Models\ProductModel;
 
 require_once __DIR__ . "/../vendor/autoload.php";
 
@@ -13,8 +14,16 @@ $cate = new CategoryModel;
 //     'slug' => "samsung",
 //     'desc' => "Điện thoại samsung"
 // ]);
-$data = [
-    'slug' => "samsung-new",
-    'desc' => "Điện thoại samsung New"
-];
-$cate->update("19 OR cate_name='blazer'", $data);
+// $data = [
+//     'slug' => "samsung-new",
+//     'desc' => "Điện thoại samsung New"
+// ];
+// $cate->update("19 OR cate_name='blazer'", $data);
+
+$product = new ProductModel;
+$result = $product->where('cate_id', '=', 2)
+    ->andWhere('price', '>', 5)
+    ->orWhere('name', '=', 'Samsung galaxy ultra')
+    ->get();
+echo "<pre>";
+var_dump($result);
