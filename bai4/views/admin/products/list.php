464d939dd8b81be796b6a0bb404a38d49e60ec4a
ngocbq3 <bqngoc@gmail.com>
2023-02-14 12:35:43 +0700

    views product list, add

---
+++ bai4/views/admin/products/list.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Danh sách sản phẩm</title>
+</head>
+
+<body>
+    <table border="1">
+        <tr>
+            <th>ID</th>
+            <th>name</th>
+            <th>Image</th>
+            <th>
+                <a href="/create-product">Thêm</a>
+            </th>
+        </tr>
+
+        <?php foreach ($products as $product) : ?>
+            <tr>
+                <td><?= $product->id ?></td>
+                <td><?= $product->name ?></td>
+                <td>
+                    <img src="images/<?= $product->image ?>" width="120" alt="">
+                </td>
+                <td>Action</td>
+            </tr>
+        <?php endforeach ?>
+    </table>
+</body>
+
+</html>
